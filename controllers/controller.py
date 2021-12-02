70cf9c495d21ae8d20fce8f264ff101d8ccd5654
Toms Druvaskalns <tomsdruvas@gmail.com>
2021-12-02 14:59:30 +0000

    create the function to add a new event and print it to the screen

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,9 +1,25 @@
-from flask import render_template
+from flask import render_template, request
 from app import app
 from models.event import Event
-from models.events import events
+from models.events import events, add_new_event
 
 
 @app.route('/events')
 def index():
     return render_template('index.html', title='Event Home', events=events)
+
+
+@app.route('/events', methods=['POST'])
+def add_event():
+    name_event = request.form['name_event']
+    date = request.form['date']
+    num_guests = request.form['num_guests']
+    location = request.form['location']
+    description = request.form['description']
+    recurring = request.form['recurring']
+    new_event = Event(date, name_event, num_guests, location, description, recurring)
+
+    add_new_event(new_event)
+    return render_template('index.html', title='Event Home', events=events)
+
+
--- models/events.py
+++ models/events.py
@@ -5,3 +5,7 @@ from models.event import *
 event1 = Event("02.12.2021", "Tree Hugging", 100, "Glasgow", "Everyone is welcome!", True)
 event2 = Event("03.12.2021", "Pre Xmas Party", 200, "Edinburgh", "Drinking before xmas starts", False)
 events = [event1, event2]
+
+
+def add_new_event(event):
+    events.append(event)
