96d8dd77becba5f1ee617677c75b1e40e904f5eb
Toms Druvaskalns <tomsdruvas@gmail.com>
2021-12-02 15:47:19 +0000

    checkbox problem

--- controllers/controller.py
+++ controllers/controller.py
@@ -16,10 +16,12 @@ def add_event():
     num_guests = request.form['num_guests']
     location = request.form['location']
     description = request.form['description']
-    recurring = request.form['recurring']
+    if request.form['recurring']:
+        recurring = True
+    else:
+        recurring = False
     new_event = Event(date, name_event, num_guests, location, description, recurring)
 
     add_new_event(new_event)
     return render_template('index.html', title='Event Home', events=events)
-
 
