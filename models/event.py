ca82e558aa23fcbe5653a35fa1871ca34c911ede
Toms Druvaskalns <tomsdruvas@gmail.com>
2021-12-02 13:48:01 +0000

    first commit

---
+++ models/event.py
@@ -0,0 +1,8 @@
+class Event():
+    def __init__(self, date, name_event, num_guests, location, description, recurring):
+        self.date = date
+        self.name_event = name_event
+        self.num_guests = num_guests
+        self.location = location
+        self.description = description
+        self.recurring = recurring
---
+++ models/events.py
@@ -0,0 +1,7 @@
+from models.event import *
+
+
+
+event1 = Event("02.12.2021", "Tree Hugging", 100, "Glasgow", "Everyone is welcome!")
+event2 = Event("03.12.2021", "Pre Xmas Party", 200, "Edinburgh", "Drinking before xmas starts")
+events = [event1, event2]
